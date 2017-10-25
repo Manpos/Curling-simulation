bde64a36345c4ceb7e55447d7f41ecd7e11e64b4
Miquel Postigo Llabres <miquelpostigo@gmail.com>
2017-10-25 11:07:05 +0200

    Initial commit

---
+++ Simulations/Assets/Scripts/CurlingStone.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class CurlingStone {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public Vector position, velocity, angularVelocity, Force;
+    public float mass, speed, orientation, radius, frictionCoeficient, restitutionCoeficent;
+    public void CalcMoment() { }
+    public void EulerStep() { }
+    public void CollisionCheck() { }
+    public void ApplyImpulse() { }
+}
---
+++ Simulations/Assets/Scripts/Vector.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vector {
+
+    public float x, y, z;
+
+    void Start() {
+
+    }
+
+    Vector(float a, float b, float c)
+    {
+        x = a;
+        y = b;
+        z = c;
+    }
+
+    public Vector cross(Vector a, Vector b)
+    {
+        return new Vector(a.y * b.z - a.z * b.y, a.z * b.x - a.x * b.z, a.x * b.y - a.y * b.x);
+    }
+
+    public float dot(Vector a, Vector b)
+    {
+        return a.x * b.x + a.y + b.y + a.z + b.z;
+    }
+
+    public Vector normalize(Vector n)
+    {
+        float module = Mathf.Sqrt((n.x * n.x) + (n.y * n.y) + (n.z * n.z));
+        return new Vector(n.x / module, n.y / module, n.z / module);
+    }
+}
