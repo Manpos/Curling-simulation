f95999f09ac1391dd9ebcb2ab64497dfcb1b776b
Miquel Postigo Llabres <miquelpostigo@gmail.com>
2017-11-22 20:11:27 +0100

    Implementació rotació (no funciona)

--- Simulations/Assets/Scripts/CurlingStone.cs
+++ Simulations/Assets/Scripts/CurlingStone.cs
@@ -5,25 +5,35 @@ using UnityEngine;
 public class CurlingStone : MonoBehaviour {
 
     public Transform t;
-    public Vector position, velocity, angularVelocity, Force;
-    public float vX, vY;
-    public float mass = 20, orientation, radius, frictionCoeficientF = 0.4f, frictionCoeficientB = 0.6f, restitutionCoeficent;
+    public float vXinit, vYinit, wZInit;
+    public float mass = 20, orientation, radius = 1.0f, frictionCoeficientF = 0.4f, frictionCoeficientB = 0.6f, restitutionCoeficent;
     public float gravity = 9.81f;
 
     // Velocity and position
     private Vector v;
     private Vector s;
 
+    // Angular velocity and rotation    
+    private Vector w;
+    private Vector r;
+
 	// Use this for initialization
 	void Start () {
         v = new Vector();
         s = new Vector();
 
+        w = new Vector();
+        r = new Vector();
+        
+
         s.x = t.position.x;
         s.y = t.position.y;
 
-        v.x = vX;
-        v.y = vY;
+        v.x = vXinit;
+        v.y = vYinit;
+
+        w.z = wZInit;
+        r.z = t.rotation.z;
 	}
 	
 	// Update is called once per frame
@@ -37,28 +47,35 @@ public class CurlingStone : MonoBehaviour {
 
     public void EulerStep(float dt) {
 
-        // New velocity, new position
+        // New velocity, new position, new angular velocity, orientation
         Vector vNew = new Vector();
         Vector sNew = new Vector();
+        Vector wNew = new Vector();
+        Vector rNew = new Vector();
 
-        // Calculate acceleration
-        Vector acceleration = new Vector();
-        acceleration.x = -((frictionCoeficientF + frictionCoeficientB) / 2) * gravity;
-        acceleration.y = (frictionCoeficientF - frictionCoeficientB) * gravity; 
+        // Calculate angular velocity
+        wNew.z = (gravity * (frictionCoeficientF - frictionCoeficientB) / radius) * dt + w.z;
+
+        // Calculate new angle
+        rNew.z = wNew.z * dt + r.z;
 
         // Calculate velocity
         vNew.x = -((frictionCoeficientF + frictionCoeficientB) / 2) * gravity * dt + v.x;
-        vNew.y = (frictionCoeficientF - frictionCoeficientB) * gravity * dt + v.y;
+        vNew.y = wNew.z * radius;
 
         // Calculate new position
-        sNew.x = s.x + v.x * dt - 0.5f * gravity * (frictionCoeficientF - frictionCoeficientB) * (dt * dt);
-        sNew.y = s.y + v.y * dt -0.5f * gravity * ((frictionCoeficientF + frictionCoeficientB)/2) * (dt * dt);
+        sNew.x = s.x + v.x * dt - 0.5f * gravity * ((frictionCoeficientF + frictionCoeficientB) / 2) * (dt * dt);
+        sNew.y = s.y + vNew.y * dt;
 
         // Update velocity and position
         s = sNew;
         v = vNew;
+        w = wNew;
+        r = rNew;
+        
 
         t.position = new Vector3(s.x, s.y,0);
+        t.rotation = new Quaternion(0, 0, 1, r.z);
 
     }
 
