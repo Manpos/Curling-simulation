a4b04017e973f7b42848e7fe03bc15cefaafeb28
Miquel Postigo Llabres <miquelpostigo@gmail.com>
2018-01-02 17:37:41 +0100

    Kinda works

--- Assets/Scripts/CurlingStone.cs
+++ Assets/Scripts/CurlingStone.cs
@@ -4,48 +4,32 @@ using UnityEngine;
 
 public static class SimulationOptions
 {
-    public static float RADIUS = 0.20f;
-    public static float HEIGHT = 1.0f;
+    public static float RADIUS = 0.145f;
+    public static float HEIGHT = 0.20f;
     public static float MASS = 20.0f;
     public static float GRAVITY = 9.81f;
-    public static float LINEAR_DRAG_COEFFICIENT = 0.5f;
-    public static float ANGULAR_DRAG_COEFFICIENT = 0.5f;
-    public static float FRICTION_FACTOR = 0.5f;
     public static float COEFFICIENT_OF_RESTITUTION = 0.8f;
-    public static float COEFFICIENT_OF_RESTITUTION_GROUND = 0.1f;
-    public static float FRICTION_COEFFICIENT_STONES = 0.1f;
     public static float FRICTION_COEFFICIENT_GROUND = 0.02f;
-    public static float CURLING_RESISTANCE_COEFFICIENT = 0.025f;
 }
 
 public class CurlingStone : MonoBehaviour {
 
-    public float mass = 20;
-    //public Matrix3x3 inertia;
-    //public Matrix3x3 inertiaInverse;
-    public float inertia;
-    public float inertiaInverse;
+    private float mass;
     public Vector position;
     public Vector velocity;
     public Vector velocityBody;
     public Vector acceleration;
     public Vector angularAcceleration;
-    public Vector angularAccelerationGlobal;
 
     public Vector angularVelocity;
-    public Vector angularVelocityGlobal;
-    public Vector eulerAngles;
-    public float speed;
     public MyQuaternion orientation;
     public Vector forces;
     public Vector moments;
-    public Matrix3x3 ieInverse;
-    public float radius;
-    public float height;
+    private float radius;
+    private float height;
 
     //Shot parameters
     public float initialVelocityX;
-    public float initialVelocityY;
 
     public float initialAngularVelocity;
     
@@ -58,9 +42,6 @@ public class CurlingStone : MonoBehaviour {
         // Set initial velocity
         velocity = new Vector();
         velocity.x = initialVelocityX;
-        velocity.y = initialVelocityY;
-        velocity.z = 0.0f;
-        speed = velocity.module();
 
         // Set initial angular velocity
         angularVelocity = new Vector();
@@ -87,35 +68,25 @@ public class CurlingStone : MonoBehaviour {
         radius = SimulationOptions.RADIUS;
         height = SimulationOptions.HEIGHT;
 
-        // Set inertia tensor
-        inertia = (1f / 2f) * mass * (radius * radius);
-        inertiaInverse = 1f / inertia;
-
-        //inertia = new Matrix3x3();
-        //inertia.m11 = (1 / 12) * mass * (3 * (radius * radius) + (height * height));
-        //inertia.m22 = inertia.m11;
-        //inertia.m33 = (1 / 2) * mass * (radius * radius);
-
-        //inertiaInverse = inertia.Inverse();
-
     }
 	
 	// Update is called once per frame
 	void Update () {
-        EulerStep(Time.deltaTime);
-		
+        EulerStep(Time.deltaTime);		
 	}
 
     public void CalcMoment() { }
 
     public void EulerStep(float dt) {
 
+        orientation = MyQuaternion.QuatToMyQuat(transform.rotation);
+
         CalculateForces();
 
         // Integrate equations of motion
 
         // Rotations
-        angularAcceleration = moments / inertia;
+        angularAcceleration = moments / mass;
 
         angularVelocity += angularAcceleration * dt;
 
@@ -146,7 +117,7 @@ public class CurlingStone : MonoBehaviour {
         forces = new Vector();
         forces = (-1f) * velocity.normalize() * (SimulationOptions.FRICTION_COEFFICIENT_GROUND * mass * (SimulationOptions.GRAVITY));
         moments = new Vector();
-        moments = -(1f) * angularVelocity.normalize() * (SimulationOptions.FRICTION_COEFFICIENT_GROUND * mass * (SimulationOptions.GRAVITY)) * radius * (Mathf.Sin(90));
+        moments = -(1f) * angularVelocity.normalize() * (SimulationOptions.FRICTION_COEFFICIENT_GROUND * mass * (SimulationOptions.GRAVITY));
 
     }
 
